d5c6d32c8eb10cf13313e4d2643b3a167458c1a4
Darth-ATA <hasbringer1007@gmail.com>
2015-05-05 17:52:34 +0200

    Hechos varios disparadores

--- disparadores.sql
+++ disparadores.sql
@@ -1,1 +1,34 @@
- 
+ CREATE OR REPLACE TRIGGER hotelGJMAnotPS
+ BEFORE INSERT ON suministro
+ FOR EACH ROW
+ DECLARE
+ 	error NUMBER;
+ BEGIN
+ 	SELECT COUNT(*) INTO error FROM hotel,suministro,proveedor
+ 		WHERE hotel.idHotel=suministro.idHotel AND
+ 			suministro.idProveedor = proveedor.idProveedor AND
+ 			hotel.ciudad IN ('Granada','Jaen','Malaga','Almeria') AND
+ 			proveedor.ciudad = 'Sevilla';
+
+ 		IF error > 0 THEN
+ 			RAISE_APPLICATION_ERROR(-20015, 'Las ciudades de Granada, Jaén, Málaga y Almería no pueden tener suministros de Sevilla');
+ 		END IF;
+ END;
+
+ CREATE OR REPLACE TRIGGER hotelCSCHnotPG
+ BEFORE INSERT ON suministro
+ FOR EACH ROW
+ DECLARE
+ 	error NUMBER;
+ BEGIN
+ 	SELECT COUNT(*) INTO error FROM hotel,suministro,proveedor
+ 		WHERE hotel.idHotel=suministro.idHotel AND
+ 			suministro.idProveedor = proveedor.idProveedor AND
+ 			hotel.ciudad IN ('Cordoba','Sevilla','Cadiz','Huelva') AND
+ 			proveedor.ciudad = 'Granada';
+
+ 		IF error > 0 THEN
+ 			RAISE_APPLICATION_ERROR(-20016, 'Las ciudades de Córdoba, Sevilla, Cádiz o Huelva no pueden tener suministros de Granada');
+ 		END IF;
+ END;
+
