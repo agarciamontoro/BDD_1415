05f9d58ac11a95fe85122c522cefca946b8db3a5
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2015-05-20 11:34:26 +0100

    Update and rename grants.sql to grantsM1.sql

---
+++ grantsM1.sql
@@ -0,0 +1,7 @@
+ --PERMISOS MAGNOS1--
+GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoEmpleado TO Magnos2,Magnos3,Magnos4;
+GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoHotel TO Magnos2,Magnos3,Magnos4;
+GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoProveedor TO Magnos2,Magnos3,Magnos4 ;
+GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoReserva TO Magnos2,Magnos3,Magnos4 ;
+GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoTiene TO Magnos2,Magnos3,Magnos4 ;
+GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoSuministro TO Magnos2,Magnos3,Magnos4 ;
