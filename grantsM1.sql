239a5db8ddaeeb51fa6f3abf3da25b0921497539
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2015-05-20 12:23:01 +0100

    Añade permisos de Artículo

--- grantsM1.sql
+++ grantsM1.sql
@@ -3,3 +3,4 @@ GRANT SELECT,UPDATE,DELETE,INSERT ON cliente TO Magnos2,Magnos3,Magnos4;
 GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoEmpleado TO Magnos2,Magnos3,Magnos4;
 GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoHotel TO Magnos2,Magnos3,Magnos4;
 GRANT SELECT,UPDATE,DELETE,INSERT ON fragmentoReserva TO Magnos2,Magnos3,Magnos4 ;
+GRANT SELECT,UPDATE,DELETE,INSERT ON articulo TO Magnos2,Magnos3,Magnos4 ;
