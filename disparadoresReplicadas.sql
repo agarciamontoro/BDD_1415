d8de7632d172d2ab509888e8bc314666e249d718
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-06-10 20:55:12 +0200

    Primera iteración sobre disparadores de tablas replicadas

---
+++ disparadoresReplicadas.sql
@@ -0,0 +1,72 @@
+CREATE OR REPLACE TRIGGER replicacionCliente
+AFTER INSERT ON cliente
+FOR EACH ROW
+BEGIN
+  INSERT INTO magnos1.cliente
+  (
+    idCliente,
+    DNI,
+    nombre,
+    telefono 
+  )
+  VALUES
+  (
+    :NEW.idCliente,
+    :NEW.DNI,
+    :NEW.nombre,
+    :NEW.telefono 
+  );
+  
+  INSERT INTO magnos3.cliente
+  (
+    idCliente,
+    DNI,
+    nombre,
+    telefono 
+  )
+  VALUES
+  (
+    :NEW.idCliente,
+    :NEW.DNI,
+    :NEW.nombre,
+    :NEW.telefono 
+  );
+  
+  INSERT INTO magnos4.cliente
+  (
+    idCliente,
+    DNI,
+    nombre,
+    telefono 
+  )
+  VALUES
+  (
+    :NEW.idCliente,
+    :NEW.DNI,
+    :NEW.nombre,
+    :NEW.telefono 
+  );
+  
+END;
+/
+
+CREATE OR REPLACE TRIGGER replicacionArticulo
+AFTER INSERT ON articulo
+FOR EACH ROW
+BEGIN
+
+  INSERT INTO magnos1.articulo
+  (
+    idArticulo,
+    nombre,
+    tipo
+  )
+  VALUES
+  (
+    :NEW.idArticulo
+    :NEW.nombre
+    :NEW.tipo
+  );
+
+END;
+/
