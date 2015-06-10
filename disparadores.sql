cfc8c7d2a83ea4cc7a78b72b05e2638b595b07e3
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-06-10 16:08:28 +0200

    Cambios para adaptar algunos disparadores a vistas y fragmentos

--- disparadores.sql
+++ disparadores.sql
@@ -39,14 +39,14 @@ BEGIN
 
 END;
 
- -- Restricción 5
+ -- Restricción 5 (Hecho) --
 CREATE OR REPLACE TRIGGER controlFechasReservas
-BEFORE INSERT ON reserva
+BEFORE INSERT ON fragmentoReserva
 FOR EACH ROW
 	WHEN :NEW.fechaEntrada >= :NEW.fechaSalida
 BEGIN
   RAISE_APPLICATION_ERROR(-20002,'La fecha de entrada es mayor que la fecha de salida')
-END; 
+END;
 
  -- Restricción 6
 CREATE OR REPLACE TRIGGER controlReservasCliente
@@ -66,49 +66,72 @@ BEGIN
   END IF;
 END;
 
--- Restricción 10 --
+-- Restricción 10 (Hecho)--
 CREATE OR REPLACE TRIGGER salarioEmpleadoNoPuedeDisminuir
-BEFORE UPDATE OF salario ON empleado 
+BEFORE UPDATE OF salario ON empleado
 FOR EACH ROW
-	WHEN NEW.salario >= OLD.salario
+	WHEN NEW.salario < OLD.salario
 BEGIN
 	RAISE_APPLICATION_ERROR(-20004,'El salario no se puede disminuir');
 END;
 
--- Restricción 12 --
+-- Restricción 12 (Hecho)--
 CREATE OR REPLACE TRIGGER precioSuministroNoMenor
-BEFORE INSERT ON suministro 
-FOR EACH ROW
-DECLARE
-	precioMinSuminAnteriores NUMBER;
-BEGIN
-	SELECT MIN(precioPorUnidad) INTO precioMinSuminAnteriores FROM suministro
-	WHERE :NEW.idArticulo = suministro.idArticulo ;
-
-	IF NEW.precioUnidad < precioMinSuminAnteriores THEN
-		RAISE_APPLICATION_ERROR(-20005,'El precio por unidad no puede ser menor respecto a otros suministros');
-	END IF;
-END;
-
--- Restricción 13 --
-CREATE 0R REPLACE TRIGGER suministroArticuloMaxDosProvincias
 BEFORE INSERT ON suministro
 FOR EACH ROW
 DECLARE
-	ciudadNuevoSuministro VARCHAR(50);
-	error NUMBER;
+    PRAGMA AUTONOMOUS_TRANSACTION;
+	precioMinSumAnteriores fragmentoSuministro.precioUnidad%TYPE;
 BEGIN
-	SELECT provincia INTO ciudadNuevoSuministro FROM proveedor
+	SELECT MIN(precioPorUnidad)
+    INTO precioMinSumAnteriores
+    FROM suministro
+	WHERE :NEW.idArticulo = suministro.idArticulo ;
+
+	IF NEW.precioUnidad < precioMinSumAnteriores THEN
+		RAISE_APPLICATION_ERROR(-20005,'El precio por unidad no puede ser menor respecto a otros suministros');
+	END IF;
+
+    COMMIT;
+END;
+
+-- Restricción 13 (Hecho)--
+CREATE OR REPLACE TRIGGER suministroArticuloMaxDosProvincias
+BEFORE INSERT ON fragmentoSuministro
+FOR EACH ROW
+DECLARE
+    PRAGMA AUTONOMOUS_TRANSACTION;
+    ciudadProveedor fragmentoProveedor.ciudad%TYPE;
+	nVecesSuministrado NUMBER;
+BEGIN
+	SELECT ciudad
+    INTO ciudadProveedor
+    FROM proveedor
 	WHERE :NEW.idProveedor = proveedor.idProveedor;
 
-	SELECT COUNT(*)	INTO error FROM suministro,proveedor
-	WHERE (suministro.idProveedor= proveedor.idProveedor
-		AND suministro.idArticulo = NEW.idArticulo
-		AND proveedor.ciudad = ciudadNuevoSuministro);
+	CASE ciudadProveedor
+        WHEN 'Granada' THEN
+            SELECT COUNT(*) INTO nVecesSuministrado
+            FROM suministro, magnos2.fragmentoProveedor
+            WHERE (suministro.idProveedor = magnos2.fragmentoProveedor.idProveedor
+                AND suministro.idArticulo = :NEW.idArticulo
+                AND magnos2.fragmentoProveedor.ciudad = ciudadProveedor );
 
-	IF error > 0
+        WHEN 'Sevilla' THEN
+            SELECT COUNT(*) INTO nVecesSuministrado
+            FROM suminstro, magnos4.fragmentoProveedor
+            WHERE (suministro.idProveedor = magnos4.fragmentoProveedor.idProveedor
+                AND suministro.idArticulo = :new.idArticulo
+                AND magnos2.fragmentoProveedor.ciudad = ciudadProveedor );
+
+        ELSE RAISE_APPLICATION_ERROR(-20006, 'Ciudad del proveedor errónea');
+    END CASE;
+
+	IF nVecesSuministrado > 1 THEN
 		RAISE_APPLICATION_ERROR(-20006,'Un artículo sólo puede ser suministrado por dos proveedores distintos');
 	END IF;
+
+    COMMIT;
 END;
 
 -- Restricciones 15 y 16 --
@@ -125,7 +148,7 @@ BEGIN
 	IF ciudadNuevoSuministro = 'Sevilla'
 		SELECT COUNT(*) INTO error FROM hotel,suministro
 	 	WHERE (:NEW.idHotel = hotel.idHotel
-	 		AND hotel.idHotel = suministro.idHotel 
+	 		AND hotel.idHotel = suministro.idHotel
 	 		AND hotel.ciudad IN ('Granada','Jaen','Malaga','Almería'));
 
 	 	IF error > 0 THEN
@@ -136,7 +159,7 @@ BEGIN
 	IF ciudadNuevoSuministro = 'Granada'
 		SELECT COUNT(*) INTO error FROM hotel,suministro
 	 	WHERE (:NEW.idHotel = hotel.idHotel
-	 		AND hotel.idHotel = suministro.idHotel 
+	 		AND hotel.idHotel = suministro.idHotel
 	 		AND hotel.ciudad IN ('Cordoba','Sevilla','Cadiz','Huelva'));
 
 	 	IF error > 0 THEN
@@ -146,7 +169,7 @@ BEGIN
 END;
 
  --Restricción 17 --
- CREATE OR REPLACE TRIGGER 
+ CREATE OR REPLACE TRIGGER
  BEFORE DELETE ON proveedor
  FOR EACH ROW
  DECLARE
@@ -155,14 +178,14 @@ END;
  	SELECT COUNT(*) INTO error FROM suministro
  		WHERE suministro.idArticulo = :OLD.idProveedor
  			AND suministro.cantidad > 0;
- 	
+
  	IF error > 0 THEN
  		RAISE_APPLICATION_ERROR(-20009, 'No se puede eliminar, la cantidad suministrada no es 0');
  	END IF;
  END;
 
  --Restricción 18 --
- CREATE OR REPLACE TRIGGER 
+ CREATE OR REPLACE TRIGGER
  BEFORE DELETE ON articulo
  FOR EACH ROW
  DECLARE
@@ -171,10 +194,8 @@ END;
  	SELECT COUNT(*) INTO error FROM suministro
  		WHERE suministro.idArticulo = :OLD.idArticulo
  			AND suministro.cantidad > 0;
- 	
+
  	IF error > 0 THEN
  		RAISE_APPLICATION_ERROR(-20010, 'No se puede eliminar, la cantidad suministrada no es 0');
  	END IF;
  END;
-
-
