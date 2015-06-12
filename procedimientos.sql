4726bf83dd28ecfb9175dfb0c1cf269cf969e923
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-06-12 23:17:06 +0200

    Incluye procedimientos 6, 8 y 13.
    Revisar 13 (falta idProveedor).

--- procedimientos.sql
+++ procedimientos.sql
@@ -1,49 +1,3 @@
--- Cambiar director hotel (está sin terminar)
-
-/*
-CREATE OR REPLACE PROCEDURE cambiarDirector (
-    arg_idHotel    dirige_fr.idHotel%TYPE,
-    arg_idEmpleado    dirige_fr.idEmpleado%TYPE ) AS
-
-    directores  NUMBER;
-    hoteles     NUMBER;
-    BEGIN
-        -- Si ya está dicho director en la tabla, abortar
-        SELECT COUNT(*) INTO directores FROM dirige_v WHERE idEmpleado=arg_idEmpleado;
-
-        IF directores > 0 THEN
-            RAISE_APPLICATION_ERROR(-24020, 'El director introducido ya dirige un hotel');
-        END IF;
-
-        -- Si no está el director pero sí el hotel, borrar director
-        SELECT COUNT(*) INTO hoteles FROM dirige_v WHERE idHotel=arg_idHotel;
-
-        IF hoteles > 0 THEN
-            DELETE FROM dirige_fr WHERE idHotel=arg_idHotel;
-        END IF;
-
-        SELECT ciudad INTO ciudadHotel FROM hotel_v WHERE idHotel=arg_idHotel;
-
-        -- Añadir director
-        CASE ciudadHotel
-            WHEN 'Cádiz' THEN
-                INSERT INTO magnos1.dirige_fr(idHotel,idEmpleado)
-                VALUES (arg_idHotel,arg_idEmpleado);
-            WHEN 'Granada'THEN
-              INSERT INTO magnos2.dirige_fr(idHotel,idEmpleado)
-              VALUES (arg_idHotel,arg_idEmpleado);
-            WHEN 'Málaga'THEN
-              INSERT INTO magnos3.dirige_fr(idHotel,idEmpleado)
-              VALUES (arg_idHotel,arg_idEmpleado);
-            WHEN 'Sevilla' THEN
-              INSERT INTO magnos4.dirige_fr(idHotel,idEmpleado)
-              VALUES (arg_idHotel,arg_idEmpleado);
-            ELSE
-                RAISE_APPLICATION_ERROR(-24010, 'Ciudad errónea');
-        END CASE;
-END;
-*/
-
 /*
 -- Baja empleado (errores)
 CREATE OR REPLACE PROCEDURE bajaEmpleado (
@@ -210,6 +164,39 @@ BEGIN
     END IF;
 END;
 
+-- 6. Cambiar el director de un hotel.
+CREATE OR REPLACE PROCEDURE cambiarDirector (
+    arg_idHotel     hotel.idHotel%TYPE,
+    arg_idDirector  hotel.idDirector%TYPE ) AS
+
+    numDirectores   NUMBER;
+    numHoteles      NUMBER;
+    ciudadHotel     hotel.ciudad%TYPE;
+BEGIN
+    -- Si el director ya dirige un hotel, error.
+    SELECT COUNT(*) INTO numDirectores
+    FROM Hotel
+    WHERE idDirector = arg_idDirector;
+
+    IF numDirectores > 0 THEN
+        RAISE_APPLICATION_ERROR(-5000, 'Este empleado ya dirige un hotel');
+    END IF;
+
+    -- Si el hotel no existe, error. Si existe, modificamos la tupla
+    -- insertando (o cambiando si ya tenía uno) su director
+    SELECT COUNT(*) INTO numHoteles
+    FROM Hotel
+    WHERE idHotel = arg_idHotel;
+
+    IF numHoteles = 0 THEN
+        RAISE_APPLICATION_ERROR(-5001, 'Este hotel no existe en la base de datos');
+    ELSE    
+        UPDATE Hotel
+        SET idDirector = arg_idDirector
+        WHERE idHotel = arg_idHotel;
+    END IF;
+END;
+
 -- 7. Dar de alta a un nuevo cliente --
 CREATE OR REPLACE PROCEDURE nuevoCliente (
     arg_idCliente    fragmentoCliente.idCliente%TYPE,
@@ -231,6 +218,71 @@ BEGIN
     END IF;
 END;
 
+-- 8. Dar de alta o actualizar una reserva
+CREATE OR REPLACE PROCEDURE actualizarReserva (
+    arg_idCliente       cliente.idCliente%TYPE,
+    arg_idHotel         hotel.idHotel%TYPE 
+    arg_tipoHab         reserva.tipoHabitacion%TYPE 
+    arg_precio          reserva.precioNoche%TYPE 
+    arg_fechaEntrada    reserva.fechaEntrada%TYPE 
+    arg_fechaSalida     reserva.fechaSalida%TYPE ) AS
+
+    numReservas NUMBER;
+    ciudadHotel hotel.ciudad%TYPE;
+BEGIN
+    -- Si el director ya dirige un hotel, error.
+    SELECT COUNT(*) INTO numReservas
+    FROM Reserva
+    WHERE   idCliente = arg_idCliente
+            AND
+            fechaEntrada = arg_fechaEntrada
+            AND
+            fechaSalida = arg_fechaSalida;
+
+    -- Si hay una reserva con los datos suministrados, basta actualizarla
+    -- en la vista. Si no, se inserta según la ciudad correspondiente a idHotel.
+    IF numReservas > 0 THEN
+        UPDATE  Reserva
+        SET     idCliente = arg_idCliente,
+                idHotel = arg_idHotel,
+                tipoHab = arg_tipoHab,
+                precio = arg_precio,
+                fechaEntrada = arg_fechaEntrada,
+                fechaSalida = arg_fechaSalida
+        WHERE   idCliente = arg_idCliente
+                AND
+                fechaEntrada = arg_fechaEntrada
+                AND
+                fechaSalida = arg_fechaSalida;
+    ELSE
+        SELECT ciudad INTO ciudadHotel
+        FROM Hotel
+        WHERE idHotel = arg_idHotel;
+
+        CASE ciudadHotel
+            WHEN 'Cádiz' OR 'Huelva' THEN
+                INSERT INTO magnos1.fragmentoReserva
+                (idCliente, idHotel, tipoHab, precio, fechaEntrada, fechaSalida, idCliente)
+                VALUES
+                (arg_idCliente, arg_idHotel, arg_tipoHab, arg_precio, arg_fechaEntrada, arg_fechaSalida, arg_idCliente);
+            WHEN 'Granada' OR 'Jaén' THEN
+                INSERT INTO magnos2.fragmentoReserva
+                (idCliente, idHotel, tipoHab, precio, fechaEntrada, fechaSalida, idCliente)
+                VALUES
+                (arg_idCliente, arg_idHotel, arg_tipoHab, arg_precio, arg_fechaEntrada, arg_fechaSalida, arg_idCliente);
+            WHEN 'Málaga' OR 'Almería' THEN
+                INSERT INTO magnos3.fragmentoReserva
+                (idCliente, idHotel, tipoHab, precio, fechaEntrada, fechaSalida, idCliente)
+                VALUES
+                (arg_idCliente, arg_idHotel, arg_tipoHab, arg_precio, arg_fechaEntrada, arg_fechaSalida, arg_idCliente);
+            WHEN 'Sevilla' OR 'Córdoba' THEN
+                INSERT INTO magnos4.fragmentoReserva
+                (idCliente, idHotel, tipoHab, precio, fechaEntrada, fechaSalida, idCliente)
+                VALUES
+                (arg_idCliente, arg_idHotel, arg_tipoHab, arg_precio, arg_fechaEntrada, arg_fechaSalida, arg_idCliente);
+        END CASE;
+    END IF;
+END;
 
 -- 9. Anular reserva (creo que está bien, revisar porfa) --
 CREATE OR REPLACE anularReserva (
@@ -327,6 +379,29 @@ BEGIN
         ELSE
             RAISE_APPLICATION_ERROR(-24010, 'Ciudad errónea');
     END CASE;
+END;
+
+-- 13. Dar de baja un suministro. ¿Falta idProveedor?
+CREATE OR REPLACE PROCEDURE bajaSuministros (
+    arg_idHotel     suministro.idHotel%TYPE,
+    arg_idArticulo  suministro.idArticulo%TYPE,
+    arg_fecha       suministro.fecha%TYPE = null ) AS
+BEGIN
+    -- Si no hay fecha, eliminamos todas las que tengan los otros dos parámetros
+    IF arg_fecha IS NULL THEN
+        DELETE FROM Suministro
+        WHERE   idHotel = arg_idHotel
+                AND
+                idArticulo = arg_idArticulo;
+    -- Si hay fecha, eliminamos esa en concreto
+    ELSE
+        DELETE FROM Suministro
+        WHERE   idHotel = arg_idHotel
+                AND
+                idArticulo = arg_idArticulo
+                AND
+                fecha = arg_fecha;
+    END IF;
 END;
 
 -- 14. Dar de alta un nuevo artículo. --
