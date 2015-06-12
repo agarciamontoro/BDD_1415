a49c7f850afc6e31e836e1983a3c7d4794f272b8
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-06-12 19:53:16 +0200

    Integridad referencial de idDirector en Hotel con respecto de Empleado

---
+++ disparadoresReferencias.sql
@@ -0,0 +1,18 @@
+-- Integridad referencial de idDirector en Hotel con respecto a Empleado --
+-- ¿Hay que mirar en qué fragmento se encuentra el idDirector? ¿Hay que insertarlo
+-- como magnosX.fragmentoEmpledao.idDirector?
+CREATE OR REPLACE TRIGGER referenciaDirector
+BEFORE INSERT OR UPDATE ON fragmentoHotel
+FOR EACH ROW
+DECLARE
+  numEmpleados NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO numEmpleados FROM empleado
+  WHERE empleado.idEmpleado = :NEW.idDirector;
+
+  IF numEmpleados = 0 THEN
+      RAISE_APPLICATION_ERROR(-31001,'Error de integridad referencial: el director especificado no existe en la base de datos');
+  END IF;
+
+END;
+/
