bb2355ed5de87d2a38102200975817894316ce55
Darth-ATA <hasbringer1007@gmail.com>
2015-06-17 00:12:52 +0200

    Añadido disparador de integridad referencial

--- disparadoresReferencias.sql
+++ disparadoresReferencias.sql
@@ -15,3 +15,21 @@ BEGIN
    END IF;
 END;
 /
+
+-- Integridad referencial de idHotel en Suministro con respecto Hotel --
+CREATE OR REPLACE TRIGGER referenciaHotel
+BEFORE INSERT OR UPDATE ON fragmentoSuministro
+FOR EACH ROW
+DECLARE
+numHoteles NUMBER;
+BEGIN
+   IF :NEW.idHotel IS NOT NULL THEN
+      SELECT COUNT(*) INTO numHoteles FROM hotel
+      WHERE hotel.idHotel = :NEW.idHotel;
+
+      IF numHoteles = 0 THEN
+         RAISE_APPLICATION_ERROR(-20301,'Error de integridad referencial: el hotel especificado no existe en la base de datos');
+      END IF;
+   END IF;
+END;
+/
